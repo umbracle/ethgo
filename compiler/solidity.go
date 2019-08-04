41ebf449f7897c69bad50fb6a6809eaa4366b68a
ferranbt <ferranbt@protonmail.com>
2019-08-04 14:13:10 +0200

    Download solidity compiler

--- compiler/solidity.go
+++ compiler/solidity.go
@@ -2,10 +2,15 @@ package compiler
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
+
+	"github.com/hashicorp/go-getter"
 )
 
 // SolcOutput is the output of the compilation.
@@ -59,4 +64,44 @@ func (s *Solidity) Compile(code string) (interface{}, error) {
 		return nil, err
 	}
 	return output, nil
+}
+
+// DownloadSolidity downloads the solidity compiler
+func DownloadSolidity(version string, dst string, renameDst bool) error {
+	url := "https://github.com/ethereum/solidity/releases/download/v" + version + "/solc-static-linux"
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client := &getter.Client{
+		Ctx:     ctx,
+		Src:     url,
+		Dst:     dst,
+		Pwd:     pwd,
+		Mode:    getter.ClientModeAny,
+		Options: []getter.ClientOption{},
+	}
+	if err := client.Get(); err != nil {
+		return err
+	}
+
+	// rename binary
+	name := "solidity"
+	if renameDst {
+		name += "-" + version
+	}
+	dstPath := filepath.Join(dst, name)
+	if err := os.Rename(filepath.Join(dst, "solc-static-linux"), dstPath); err != nil {
+		return err
+	}
+
+	// make binary executable
+	if err := os.Chmod(dstPath, 0755); err != nil {
+		return err
+	}
+	return nil
 }
--- scripts/circleci.sh
+++ scripts/circleci.sh
@@ -2,13 +2,8 @@
 
 set -o errexit
 
-install_solidity() {
-    VERSION="0.5.5"
-    DOWNLOAD=https://github.com/ethereum/solidity/releases/download/v${VERSION}/solc-static-linux
-
-    curl -L $DOWNLOAD > /tmp/solc
-    chmod +x /tmp/solc
-    mv /tmp/solc /usr/local/bin/solc
+install_geth () {
+    # TODO
 }
 
 "$@"
