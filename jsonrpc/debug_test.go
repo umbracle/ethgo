66558cb22224bb249c3c9ac89cdcb5bbbcea8c42
Ferran Borreguero <ferranbt@protonmail.com>
2021-08-15 17:10:04 +0200

    Add debug endpoint

---
+++ jsonrpc/debug_test.go
@@ -0,0 +1,27 @@
+package jsonrpc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/umbracle/go-web3/testutil"
+)
+
+func TestDebug_TraceTransaction(t *testing.T) {
+	s := testutil.NewTestServer(t, nil)
+	defer s.Close()
+
+	c, _ := NewClient(s.HTTPAddr())
+
+	cc := &testutil.Contract{}
+	cc.AddEvent(testutil.NewEvent("A").Add("address", true))
+	cc.EmitEvent("setA", "A", addr0.String())
+
+	_, addr := s.DeployContract(cc)
+	r := s.TxnTo(addr, "setA2")
+
+	trace, err := c.Debug().TraceTransaction(r.TransactionHash)
+	assert.NoError(t, err)
+	assert.Greater(t, trace.Gas, uint64(20000))
+	assert.NotEmpty(t, trace.StructLogs)
+}
