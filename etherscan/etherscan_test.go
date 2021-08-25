f0dfd5ed56ce7bf71279401f67767a5a623823f5
Ferran Borreguero <ferranbt@protonmail.com>
2021-08-25 21:15:48 +0200

    Add etherscan test (#90)

--- etherscan/etherscan_test.go
+++ etherscan/etherscan_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/umbracle/go-web3"
 )
 
 func testEtherscanMainnet(t *testing.T) *Etherscan {
@@ -27,4 +28,14 @@ func TestGetBlockByNumber(t *testing.T) {
 	b, err := e.GetBlockByNumber(1, false)
 	assert.NoError(t, err)
 	assert.Equal(t, b.Number, uint64(1))
+}
+
+func TestContract(t *testing.T) {
+	e := testEtherscanMainnet(t)
+
+	// uniswap v2. router
+	code, err := e.GetContractCode(web3.HexToAddress("0x7a250d5630b4cf539739df2c5dacb4c659f2488d"))
+	assert.NoError(t, err)
+	assert.Equal(t, code.Runs, "999999")
+
 }
