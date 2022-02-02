51e43f9a3c2799d37a70b5ecafe2a41afb315c9f
Miloš Živković <milos.zivkovic@mvpworkshop.co>
2022-02-02 10:28:03 +0100

    Correct the topic field in log filters (#147)
    * Correct the topic field in log filters
    
    * Pipe everything into hash directly
    
    * Add coverage tests

---
+++ structs_marshal_test.go
@@ -0,0 +1,95 @@
+package web3
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func generateHashPtr(input string) *Hash {
+	res := HexToHash(input)
+	return &res
+}
+
+func TestLogFilter_MarshalJSON(t *testing.T) {
+	testTable := []struct {
+		name   string
+		topics [][]*Hash
+	}{
+		{
+			"match any topic",
+			[][]*Hash{
+				nil,
+				{},
+			},
+		},
+		{
+			"match single topic in pos. 1",
+			[][]*Hash{
+				{
+					generateHashPtr("0xa"),
+				},
+			},
+		},
+		{
+			"match single topic in pos. 2",
+			[][]*Hash{
+				{},
+				{
+					generateHashPtr("0xb"),
+				},
+			},
+		},
+		{
+			"match topic in pos. 1 AND pos. 2",
+			[][]*Hash{
+				{
+					generateHashPtr("0xa"),
+				},
+				{
+					generateHashPtr("0xb"),
+				},
+			},
+		},
+		{
+			"match topic A or B in pos. 1 AND C or D in pos. 2",
+			[][]*Hash{
+				{
+					generateHashPtr("0xa"),
+					generateHashPtr("0xb"),
+				},
+				{
+					generateHashPtr("0xc"),
+					generateHashPtr("0xd"),
+				},
+			},
+		},
+	}
+
+	defaultLogFilter := &LogFilter{
+		Address:   []Address{HexToAddress("0x123")},
+		Topics:    nil,
+		BlockHash: generateHashPtr("0xabc"),
+	}
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			defaultLogFilter.Topics = testCase.topics
+
+			// Marshal it to JSON
+			output, marshalErr := defaultLogFilter.MarshalJSON()
+			if marshalErr != nil {
+				t.Fatalf("Unable to marshal value, %v", marshalErr)
+			}
+
+			// Unmarshal it from JSON
+			reverseOutput := &LogFilter{}
+			unmarshalErr := json.Unmarshal(output, reverseOutput)
+			if unmarshalErr != nil {
+				t.Fatalf("Unable to unmarshal value, %v", unmarshalErr)
+			}
+
+			// Assert that the original and unmarshalled values match
+			assert.Equal(t, defaultLogFilter, reverseOutput)
+		})
+	}
+}
