caee3752c546a0eb91d7d86581b6cabbb97b1635
Ferran Borreguero <ferranbt@protonmail.com>
2022-02-15 15:33:27 +0100

    Rebase

--- abi/revert_test.go
+++ abi/revert_test.go
@@ -9,7 +9,10 @@ import (
 func TestUnpackRevertError(t *testing.T) {
 	data := "08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000d72657665727420726561736f6e00000000000000000000000000000000000000"
 
-	reason, err := UnpackRevertError(decodeHex(data))
+	raw, err := decodeHex(data)
+	assert.NoError(t, err)
+
+	reason, err := UnpackRevertError(raw)
 	assert.NoError(t, err)
 	assert.Equal(t, "revert reason", reason)
 }
