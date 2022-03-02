432e2c0473073594a172c18f001b5eb579a92a02
Ferran Borreguero <ferranbt@protonmail.com>
2022-03-02 17:15:43 +0100

    Remove node modules

--- website/node_modules/next/dist/client/vitals.d.ts
+++
@@ -1,6 +0,0 @@
-import { NextWebVitalsMetric } from '../pages/_app';
-declare type ReportWebVitalsCallback = (webVitals: NextWebVitalsMetric) => any;
-export declare const webVitalsCallbacks: Set<ReportWebVitalsCallback>;
-export declare function trackWebVitalMetric(metric: NextWebVitalsMetric): void;
-export declare function useWebVitalsReport(callback: ReportWebVitalsCallback): void;
-export {};
--- website/node_modules/react-innertext/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-interface InnerText {
-    (jsx: ReactNode): string;
-    default: InnerText;
-}
-declare const innerText: InnerText;
-export = innerText;
