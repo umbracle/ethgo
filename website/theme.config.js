913c16d1c02ac21db7d77da9dfb27c48648e1e48
Ferran Borreguero <ferranbt@protonmail.com>
2022-03-03 10:17:44 +0100

    Fix github edit link

--- website/theme.config.js
+++ website/theme.config.js
@@ -1,6 +1,6 @@
 export default {
     projectLink: 'https://github.com/umbracle/ethgo', // GitHub link in the navbar
-    docsRepositoryBase: 'https://github.com/umbracle/ethgo/blob/master/website', // base URL for the docs repository
+    docsRepositoryBase: 'https://github.com/umbracle/ethgo/tree/master/website/pages', // base URL for the docs repository
     titleSuffix: ' – Ethgo',
     nextLinks: true,
     prevLinks: true,
