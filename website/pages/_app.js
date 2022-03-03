1621d9ff042b885412c7db1335f885b209349744
Ferran Borreguero <ferranbt@protonmail.com>
2022-03-03 10:36:17 +0100

    Add plausible web tracker (#163)

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -4,6 +4,15 @@ import Prism from 'prism-react-renderer/prism'
 (typeof global !== "undefined" ? global : window).Prism = Prism
 require("prismjs/components/prism-go")
 
+import Head from 'next/head';
+
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script defer data-domain="ethgo.dev" src="https://plausible.io/js/plausible.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
