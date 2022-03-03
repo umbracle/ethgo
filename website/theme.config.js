52a2bbe3b7428857f80cc2a7719e58c383a6f192
Ferran Borreguero <ferranbt@protonmail.com>
2022-03-03 10:18:44 +0100

    Fix text header

--- website/theme.config.js
+++ website/theme.config.js
@@ -22,8 +22,8 @@ export default {
     head: (
         <>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta name="description" content="Ethgo: the next docs builder" />
-            <meta name="og:title" content="Ethgo: the next docs builder" />
+            <meta name="description" content="Ethgo: lightweight Go Ethereum SDK" />
+            <meta name="og:title" content="Ethgo: lightweight Go Ethereum SDK" />
         </>
     ),
 }
