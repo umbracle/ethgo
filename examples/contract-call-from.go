c23dcd7458f1985cea3dd00e8acfc78db6d68125
Ferran Borreguero <ferranbt@protonmail.com>
2022-03-10 13:24:57 +0100

    Update docs

---
+++ examples/contract-call-from.go
@@ -0,0 +1,36 @@
+package examples
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/umbracle/ethgo"
+	"github.com/umbracle/ethgo/abi"
+	"github.com/umbracle/ethgo/contract"
+	"github.com/umbracle/ethgo/jsonrpc"
+)
+
+// call a contract and use a custom `from` address
+func contractCallFrom() {
+	var functions = []string{
+		"function totalSupply() view returns (uint256)",
+	}
+
+	abiContract, err := abi.NewABIFromList(functions)
+	handleErr(err)
+
+	// Matic token
+	addr := ethgo.HexToAddress("0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0")
+
+	client, err := jsonrpc.NewClient("https://mainnet.infura.io")
+	handleErr(err)
+
+	// from address (msg.sender in solidity)
+	from := ethgo.Address{0x1}
+
+	c := contract.NewContract(addr, abiContract, contract.WithSender(from), contract.WithJsonRPC(client.Eth()))
+	res, err := c.Call("totalSupply", ethgo.Latest)
+	handleErr(err)
+
+	fmt.Printf("TotalSupply: %s", res["totalSupply"].(*big.Int))
+}
