b9cf7117a820ca9e4eaf6bcb15b15c56077221f7
Ferran <ferranbt@protonmail.com>
2022-03-31 14:18:45 +0400

    Add ethers.js testcases

--- wallet/key_test.go
+++ wallet/key_test.go
@@ -1,9 +1,13 @@
 package wallet
 
 import (
+	"encoding/hex"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/umbracle/ethgo"
+	"github.com/umbracle/ethgo/testsuite"
 )
 
 func TestKeySign(t *testing.T) {
@@ -17,4 +21,31 @@ func TestKeySign(t *testing.T) {
 	addr, err := EcrecoverMsg(msg, signature)
 	assert.NoError(t, err)
 	assert.Equal(t, addr, key.addr)
+}
+
+func TestSpec_Accounts(t *testing.T) {
+	var walletSpec []struct {
+		Address    string  `json:"address"`
+		Checksum   string  `json:"checksumAddress"`
+		Name       string  `json:"name"`
+		PrivateKey *string `json:"privateKey,omitempty"`
+	}
+	testsuite.ReadTestCase(t, "accounts", &walletSpec)
+
+	for _, spec := range walletSpec {
+		if spec.PrivateKey != nil {
+			// test that we can decode the private key
+			priv, err := hex.DecodeString(strings.TrimPrefix(*spec.PrivateKey, "0x"))
+			assert.NoError(t, err)
+
+			key, err := NewWalletFromPrivKey(priv)
+			assert.NoError(t, err)
+
+			assert.Equal(t, key.Address().String(), spec.Checksum)
+		}
+
+		// test that an string address can be checksumed
+		addr := ethgo.HexToAddress(spec.Address)
+		assert.Equal(t, addr.String(), spec.Checksum)
+	}
 }
