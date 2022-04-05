b441a422ff4de37a1168ce6250df2a09948aeaba
Ferran <ferranbt@protonmail.com>
2022-04-05 17:40:48 +0400

    Add 4byte

---
+++ 4byte/4byte.go
@@ -0,0 +1,51 @@
+package fourbyte
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	fourByteURL = "https://www.4byte.directory"
+)
+
+// Resolve resolves a method/event signature
+func Resolve(str string) (string, error) {
+	return get("/api/v1/signatures/?hex_signature=" + str)
+}
+
+// ResolveBytes resolves a method/event signature in bytes
+func ResolveBytes(b []byte) (string, error) {
+	return Resolve(hex.EncodeToString(b))
+}
+
+func get(path string) (string, error) {
+	req, err := http.Get(fourByteURL + path)
+	if err != nil {
+		return "", err
+	}
+	defer req.Body.Close()
+
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var result struct {
+		Results []signatureResult
+	}
+	if err := json.Unmarshal(data, &result); err != nil {
+		return "", err
+	}
+
+	if len(result.Results) == 0 {
+		return "", nil
+	}
+	return result.Results[0].TextSignature, nil
+}
+
+type signatureResult struct {
+	TextSignature string `json:"text_signature"`
+}
