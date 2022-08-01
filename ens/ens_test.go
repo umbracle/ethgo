6bd2ce013a52616081c6330732fa2467f4c21d34
RoCry <crysheen@gmail.com>
2022-08-01 12:33:03 +0800

    ENS supports reverse resolve

--- ens/ens_test.go
+++ ens/ens_test.go
@@ -12,7 +12,11 @@ func TestENS_Resolve(t *testing.T) {
 	ens, err := NewENS(WithAddress(testutil.TestInfuraEndpoint(t)))
 	assert.NoError(t, err)
 
-	addr, err := ens.Resolve("arachnid.eth")
+	addr, err := ens.Resolve("nick.eth")
 	assert.NoError(t, err)
 	assert.Equal(t, ethgo.HexToAddress("0xb8c2C29ee19D8307cb7255e1Cd9CbDE883A267d5"), addr)
+
+	name, err := ens.ReverseResolve(ethgo.HexToAddress("0xb8c2C29ee19D8307cb7255e1Cd9CbDE883A267d5"))
+	assert.NoError(t, err)
+	assert.Equal(t, "nick.eth", name)
 }
