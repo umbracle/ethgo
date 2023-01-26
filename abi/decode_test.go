192f1234e34b4dd0b2e2a3c811c8d819434d8f0b
Ferran Borreguero <ferranbt@protonmail.com>
2023-01-26 10:23:30 +0100

    Fix abi decode dynamic length items

--- abi/decode_test.go
+++ abi/decode_test.go
@@ -1,14 +1,20 @@
 package abi
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestDecode_BytesBound(t *testing.T) {
 	typ := MustNewType("tuple(string)")
 	decodeTuple(typ, nil) // it should not panic
 }
 
-func TestDecode_Unbound(t *testing.T) {
-	input := []byte("00000000000000000000000000000000\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0000000000000000000000000000")
-	inputDataABIType := MustNewType("tuple(bytes32, bytes, bytes)")
-	_, _ = Decode(inputDataABIType, input)
+func TestDecode_DynamicLengthOutOfBounds(t *testing.T) {
+	input := []byte("00000000000000000000000000000000\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 00000000000000000000000000")
+	typ := MustNewType("tuple(bytes32, bytes, bytes)")
+
+	_, err := Decode(typ, input)
+	require.Error(t, err)
 }
