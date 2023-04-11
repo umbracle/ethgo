6e8d38bb51c037808b569207679d602955d33714
Ferran Borreguero <ferranbt@protonmail.com>
2023-04-11 11:13:14 +0200

    Add rest of types for eip-712 (#243)

--- signing/eip712_test.go
+++ signing/eip712_test.go
@@ -1,6 +1,7 @@
 package signing
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,6 +10,7 @@ import (
 
 type Message struct {
 	A    uint64      `eip712:"a"`
+	C    *big.Int    `eip712:"c"`
 	Msg1 *Message2   `eip712:"msg1"`
 	Msg2 []Message2  `eip712:"msg2"`
 	Msg3 [3]Message2 `eip712:"msg3"`
@@ -19,15 +21,16 @@ type Message2 struct {
 	Addr ethgo.Address `eip712:"addr"`
 }
 
-func TestBuildMessage(t *testing.T) {
+func TestBuildMessage_Encode(t *testing.T) {
 	domain := &EIP712Domain{
 		Name: "name1",
 	}
 
 	b := NewEIP712MessageBuilder[Message](domain)
-	require.Equal(t, "Message(uint64 a,Message2 msg1,Message2[] msg2,Message2[3] msg3)Message2(uint64 b,address addr)", b.GetEncodedType())
+	require.Equal(t, "Message(uint64 a,uint256 c,Message2 msg1,Message2[] msg2,Message2[3] msg3)Message2(uint64 b,address addr)", b.GetEncodedType())
 
 	msg := &Message{
+		C:    big.NewInt(1),
 		Msg1: &Message2{},
 		Msg2: []Message2{
 			{B: 1},
@@ -46,4 +49,22 @@ func TestBuildMessage(t *testing.T) {
 
 	_, err := typedMsg.Hash()
 	require.NoError(t, err)
+}
+
+func TestBuildMessage_BasicTypes(t *testing.T) {
+	domain := &EIP712Domain{
+		Name: "name1",
+	}
+
+	type Message struct {
+		A uint64
+		B uint32
+		C uint16
+		D uint8
+		E [32]byte
+		F string
+	}
+
+	b := NewEIP712MessageBuilder[Message](domain)
+	require.Equal(t, "Message(uint64 A,uint32 B,uint16 C,uint8 D,[32]byte E,string F)", b.GetEncodedType())
 }
