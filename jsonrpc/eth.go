ec7ffb39358a1a05a128f30af5eb58d128fe04b8
Stefan Negovanović <stefan@ethernal.tech>
2023-08-07 15:18:02 +0200

    Expose eth_maxPriorityFeePerGas function

--- jsonrpc/eth.go
+++ jsonrpc/eth.go
@@ -273,3 +273,14 @@ func (e *Eth) FeeHistory(from, to ethgo.BlockNumber) (*FeeHistory, error) {
 	}
 	return out, nil
 }
+
+// MaxPriorityFeePerGas returns a fee per gas that is an estimate of how much you can pay as a priority fee, or 'tip',
+// to get a transaction included in the current block (EIP-1559).
+func (e *Eth) MaxPriorityFeePerGas() (*big.Int, error) {
+	var out string
+	if err := e.c.Call("eth_maxPriorityFeePerGas", &out); err != nil {
+		return big.NewInt(0), err
+	}
+
+	return parseBigInt(out), nil
+}
