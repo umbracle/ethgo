1b3ffd64c26339930cacc6805366b57352a26c27
begmaroman <begmaroman@gmail.com>
2023-10-05 16:48:54 +0800

    Minor updates

--- jsonrpc/debug.go
+++ jsonrpc/debug.go
@@ -12,13 +12,13 @@ func (c *Client) Debug() *Debug {
 }
 
 type TraceTransactionOptions struct {
-	EnableMemory     bool
-	DisableStack     bool
-	DisableStorage   bool
-	EnableReturnData bool
-	Timeout          string
-	Tracer           string
-	TracerConfig     map[string]interface{}
+	EnableMemory     bool                   `json:"enableMemory"`
+	DisableStack     bool                   `json:"disableStack"`
+	DisableStorage   bool                   `json:"disableStorage"`
+	EnableReturnData bool                   `json:"enableReturnData"`
+	Timeout          string                 `json:"timeout,omitempty"`
+	Tracer           string                 `json:"tracer,omitempty"`
+	TracerConfig     map[string]interface{} `json:"tracerConfig,omitempty"`
 }
 
 type TransactionTrace struct {
@@ -38,44 +38,8 @@ type StructLogs struct {
 	Storage map[string]string
 }
 
-func (d *Debug) TraceTransaction(hash ethgo.Hash, opts *TraceTransactionOptions) (*TransactionTrace, error) {
+func (d *Debug) TraceTransaction(hash ethgo.Hash, opts TraceTransactionOptions) (*TransactionTrace, error) {
 	var res *TransactionTrace
-	err := d.c.Call("debug_traceTransaction", &res, hash, toTraceTransactionOpts(opts))
+	err := d.c.Call("debug_traceTransaction", &res, hash, opts)
 	return res, err
-}
-
-func toTraceTransactionOpts(opts *TraceTransactionOptions) map[string]interface{} {
-	optsMap := make(map[string]interface{})
-
-	if opts != nil {
-		if opts.EnableMemory {
-			optsMap["enableMemory"] = true
-		}
-
-		if opts.DisableStack {
-			optsMap["disableStack"] = true
-		}
-
-		if opts.DisableStorage {
-			optsMap["disableStorage"] = true
-		}
-
-		if opts.EnableReturnData {
-			optsMap["enableReturnData"] = true
-		}
-
-		if opts.Timeout != "" {
-			optsMap["timeout"] = opts.Timeout
-		}
-
-		if opts.Tracer != "" {
-			optsMap["tracer"] = opts.Tracer
-
-			if len(opts.TracerConfig) > 0 {
-				optsMap["tracerConfig"] = opts.TracerConfig
-			}
-		}
-	}
-
-	return optsMap
 }
